174412c98e533689ba9c382f403c92bf464caac0
oliverde8 <oliverde8@gmail.com>
2017-05-21 13:11:59 +0200

    Added finishing touches to documentation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,16 @@ $( document ).ready(function() {
     $('.doc-content table').addClass('uk-table uk-table-divider');
     $('.doc-content blockquote').addClass('uk-alert-primary');
 
+    tocbot.init({
+        tocSelector: '#doc_nav',
+        contentSelector: '#documentation',
+        headingSelector: 'h1, h2, h3, h4',
+
+        listClass: 'uk-nav',
+        activeLinkClass: 'exp-active'
+    });
+
+    $('#doc_nav > ul').addClass('uk-nav-default');
+    $('#doc_nav > ul > li ul').addClass('uk-nav-sub');
+
 });
